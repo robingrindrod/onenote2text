84117da3048322943e58d401798ed90ce23bb0bc
Robin Grindrod <robingrindrod@live.ie>
2016-12-16 13:32:32 +0530

    Add the ability to get notbook sections
    Promts the user to select the notbook that they would like to export and
    then fetches and prints the sections of that notbook.

--- onenote2text.py
+++ onenote2text.py
@@ -23,7 +23,15 @@ print(authorization_url)
 response = raw_input('Paste the URL you were redicted to here:')
 token = session.token_from_fragment(response)
 
-response = session.get(API_ROOT_URL + '/notebooks')
-notebooks = json.loads(response.text)['value']
-for notebook in notebooks:
-    print(notebook['name'])
+params = {'select': 'id,name'}
+notebooks_response = session.get(API_ROOT_URL + '/notebooks', params=params)
+notebooks = json.loads(notebooks_response.text)['value']
+for num, notebook in enumerate(notebooks, start=1):
+    print(str(num) + '. ' + notebook['name'])
+
+notebook_num = int(raw_input('Enter the number of the notebook to convert:'))
+notebook = notebooks[notebook_num - 1]
+sections_response = session.get(API_ROOT_URL + '/notebooks/' + notebook['id'] + '/sections', params=params)
+sections = json.loads(sections_response.text)['value']
+for section in sections:
+    print(section['name'])
