cfcf80098f3a1419b541f7b4fa2dda60a903d24f
Robin Grindrod <robingrindrod@live.ie>
2016-12-22 16:58:07 +0530

    Refactor script into seperate methods

--- onenote2text.py
+++ onenote2text.py
@@ -14,24 +14,39 @@ AUTHORIZATION_BASE_URL = 'https://login.live.com/oauth20_authorize.srf'
 REDIRECT_URI = 'https://login.live.com/oauth20_desktop.srf'
 SCOPE = ['wl.signin', 'office.onenote']
 
-session = OAuth2Session(client=MobileApplicationClient(client_id=CLIENT_ID),
-        client_id=CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
-authorization_url = session.authorization_url(AUTHORIZATION_BASE_URL)[0]
+def get_token_from_user(authorization_url, session):
+    print('Visit this address in your browser:')
+    print(authorization_url)
+    response = raw_input('Paste the URL you were redicted to here:')
+    session.token_from_fragment(response)
 
-print('Visit this address in your browser:')
-print(authorization_url)
-response = raw_input('Paste the URL you were redicted to here:')
-token = session.token_from_fragment(response)
+def get_notebook_from_user(notebooks):
+    for num, notebook in enumerate(notebooks, start=1):
+        print(str(num) + '. ' + notebook['name'])
+    notebook_num = int(raw_input('Enter the number of the notebook to convert:'))
+    return notebooks[notebook_num - 1]
 
-params = {'select': 'id,name'}
-notebooks_response = session.get(API_ROOT_URL + '/notebooks', params=params)
-notebooks = json.loads(notebooks_response.text)['value']
-for num, notebook in enumerate(notebooks, start=1):
-    print(str(num) + '. ' + notebook['name'])
+def get_authenticated_session():
+    session = OAuth2Session(client=MobileApplicationClient(client_id=CLIENT_ID),
+            client_id=CLIENT_ID, redirect_uri=REDIRECT_URI, scope=SCOPE)
+    authorization_url = session.authorization_url(AUTHORIZATION_BASE_URL)[0]
+    get_token_from_user(authorization_url, session)
+    return session
 
-notebook_num = int(raw_input('Enter the number of the notebook to convert:'))
-notebook = notebooks[notebook_num - 1]
-sections_response = session.get(API_ROOT_URL + '/notebooks/' + notebook['id'] + '/sections', params=params)
-sections = json.loads(sections_response.text)['value']
+def get_notebooks(session):
+    return get_object(session, API_ROOT_URL + '/notebooks')
+
+def get_sections(session, notebook_id):
+    return get_object(session, API_ROOT_URL + '/notebooks/' + notebook_id + '/sections')
+
+def get_object(session, url):
+    params = {'select': 'id,name'}
+    response = session.get(url, params=params)
+    return json.loads(response.text)['value']
+
+session = get_authenticated_session()
+notebooks = get_notebooks(session)
+notebook = get_notebook_from_user(notebooks)
+sections = get_sections(session, notebook['id'])
 for section in sections:
     print(section['name'])
